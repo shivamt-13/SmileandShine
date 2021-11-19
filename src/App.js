4c0151a66fd100134c40e1065ca025f37693b352
Rolit19 <rolit454@gmail.com>
2021-11-19 14:30:48 +0530

    Carousel added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import Header from '../src/components/header/Navbar'
 import Footer from '../src/components/footer/Footer'
+import Carousel from '../src/components/elements/Carousel'
 
 function App() {
   return (
     <>
       <Header/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <Carousel/>
       <Footer/>
     </>
   );
---
+++ src/components/elements/Carousel.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { CCarousel,CCarouselItem,CImage } from '@coreui/react'
+import './Carousel.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import img1 from "../../assets/poor1.jpeg";
+import img2 from "../../assets/poor2.jpeg";
+import img3 from "../../assets/poor5.jpeg";
+const App = () => {
+  return (
+    <div className="Carousel">
+    <CCarousel controls transition="crossfade" style={{width="96%",marginTop:"90px"}}>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src={img1} alt="slide 1" />
+        </CCarouselItem>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src={img2} alt="slide 2" />
+        </CCarouselItem>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src={img3} alt="slide 3" />
+        </CCarouselItem>
+    </CCarousel>
+    </div>
+  );
+};
+export default App;
