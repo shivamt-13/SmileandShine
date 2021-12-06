13a15cebfb33174f500e4c330888d224ca53179c
jatin831 <jatinbajaj2001@gmail.com>
2021-12-06 19:20:35 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Footer from '../src/components/footer/Footer'
 import LandingPage from './components/Landingpage';
 import Vision from './components/Vision';
 import Aboutus from './components/Aboutus';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header/>
-      {/* <LandingPage/> */}
-      <Aboutus/>
-      {/* <Vision/> */}
+      <Routes>
+        <Route path="/" element={<LandingPage />}/>
+        <Route path="/about-us" element={<Aboutus />}/>
+        <Route path="/vision" element={<Vision />}/>
+      </Routes>
       <Footer/>
     </>
   );
